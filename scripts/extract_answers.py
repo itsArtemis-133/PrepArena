07ca544a16c238680ae571542adb4daccda98d30
Siddharth Mishra <siddharth344tata@gmail.com>
2025-09-10 18:39:55 +0530

    Added PORT for universal run Changes for Railway in extractor

--- scripts/extract_answers.py
+++ scripts/extract_answers.py
@@ -96,4 +96,5 @@ def extract():
             os.remove(temp_path)
 
 if __name__ == '__main__':
-    app.run(port=8001, debug=True)
+    port = int(os.environ.get("PORT", 8001))  # default 8001 locally
+    app.run(host="0.0.0.0", port=port, debug=True)
